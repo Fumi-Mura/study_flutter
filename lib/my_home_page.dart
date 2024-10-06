42bd6f99a5fa0ba95b01ca90225cb5af6bd0c594
iret_muramatsu <fumiaki.muramatsu@uni.iret.co.jp>
2024-10-06 16:54:02 +0900

    List

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -18,108 +18,101 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  List fruite = ["りんご", "みかん", "ブドウ"];
+
   @override
   Widget build(BuildContext context) {
+    // print(fruite[1]); // consoleに出力
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // const SizedBox(
-            //   height: 50,
-            //   width: 50,
-            //   child: ColoredBox(color: Colors.blue)
-            // ),
-            // const SizedBox(
-            //   height: 50,
-            //   width: 50,
-            //   child: ColoredBox(color: Colors.blue)
-            // ),
-            // const SizedBox(
-            //   height: 10,
-            //   width: 100,
-            //   child: ColoredBox(color: Colors.yellow)
-            // ),
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            const Row(
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: ColoredBox(color: Colors.blue),
-                  ),
-                ),
-                SizedBox(
-                  height: 50,
-                  width: 50,
-                  child: ColoredBox(color: Colors.blue),
-                ),
-              ],
-            ),
-            const Row(
-              // mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: ColoredBox(color: Colors.pink),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: ColoredBox(color: Colors.pink),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 50,
-                    width: 50,
-                    child: ColoredBox(color: Colors.pink),
-                  ),
-                ),
-              ],
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: 80,
-                    width: 80,
-                    child: ColoredBox(color: Colors.green),
-                  ),
-                ),
-                SizedBox(
-                  height: 80,
-                  width: 80,
-                  child: ColoredBox(color: Colors.green),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
+      body: ListView.builder(
+        itemCount: fruite.length,
+        itemBuilder: (context, index){
+          return Text(fruite[index]);
+      }),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisSize: MainAxisSize.min,
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         'You have pushed the button this many times:',
+      //       ),
+      //       Text(
+      //         '$_counter',
+      //         style: Theme.of(context).textTheme.headlineMedium,
+      //       ),
+      //       const Row(
+      //         children: <Widget>[
+      //           Padding(
+      //             padding: EdgeInsets.all(8.0),
+      //             child: SizedBox(
+      //               height: 50,
+      //               width: 50,
+      //               child: ColoredBox(color: Colors.blue),
+      //             ),
+      //           ),
+      //           SizedBox(
+      //             height: 50,
+      //             width: 50,
+      //             child: ColoredBox(color: Colors.blue),
+      //           ),
+      //         ],
+      //       ),
+      //       const Row(
+      //         // mainAxisSize: MainAxisSize.min,
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: <Widget>[
+      //           Padding(
+      //             padding: EdgeInsets.all(8.0),
+      //             child: SizedBox(
+      //               height: 50,
+      //               width: 50,
+      //               child: ColoredBox(color: Colors.pink),
+      //             ),
+      //           ),
+      //           Padding(
+      //             padding: EdgeInsets.all(8.0),
+      //             child: SizedBox(
+      //               height: 50,
+      //               width: 50,
+      //               child: ColoredBox(color: Colors.pink),
+      //             ),
+      //           ),
+      //           Padding(
+      //             padding: EdgeInsets.all(8.0),
+      //             child: SizedBox(
+      //               height: 50,
+      //               width: 50,
+      //               child: ColoredBox(color: Colors.pink),
+      //             ),
+      //           ),
+      //         ],
+      //       ),
+      //       const Row(
+      //         mainAxisAlignment: MainAxisAlignment.end,
+      //         children: <Widget>[
+      //           Padding(
+      //             padding: EdgeInsets.all(8.0),
+      //             child: SizedBox(
+      //               height: 80,
+      //               width: 80,
+      //               child: ColoredBox(color: Colors.green),
+      //             ),
+      //           ),
+      //           SizedBox(
+      //             height: 80,
+      //             width: 80,
+      //             child: ColoredBox(color: Colors.green),
+      //           ),
+      //         ],
+      //       ),
+      //     ],
+      //   ),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
